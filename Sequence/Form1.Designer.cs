7ae925d3aebd6b7d388bfb4f68f7e9c78fc78163
yayayaka <yayayaka@yandex.ru>
2019-01-07 02:21:38 +0400

    first commit, validation is almost completу

---
+++ Sequence/Form1.Designer.cs
@@ -0,0 +1,198 @@
+﻿namespace Sequence
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.enterSequenceTextBox = new System.Windows.Forms.TextBox();
+            this.inputSequenceLabel = new System.Windows.Forms.Label();
+            this.resultLabel = new System.Windows.Forms.Label();
+            this.resultTextBox = new System.Windows.Forms.TextBox();
+            this.getResultButton = new System.Windows.Forms.Button();
+            this.enterU0370Button = new System.Windows.Forms.Button();
+            this.enterU02C4Button = new System.Windows.Forms.Button();
+            this.enterU02C5Button = new System.Windows.Forms.Button();
+            this.enterU00ACButton = new System.Windows.Forms.Button();
+            this.enterU2192Button = new System.Windows.Forms.Button();
+            this.sequenceValidationLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // enterSequenceTextBox
+            // 
+            this.enterSequenceTextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.enterSequenceTextBox.Location = new System.Drawing.Point(23, 42);
+            this.enterSequenceTextBox.Name = "enterSequenceTextBox";
+            this.enterSequenceTextBox.Size = new System.Drawing.Size(401, 39);
+            this.enterSequenceTextBox.TabIndex = 0;
+            this.enterSequenceTextBox.TextChanged += new System.EventHandler(this.enterSequenceTextBox_TextChanged);
+            // 
+            // inputSequenceLabel
+            // 
+            this.inputSequenceLabel.AutoSize = true;
+            this.inputSequenceLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.inputSequenceLabel.Location = new System.Drawing.Point(23, 13);
+            this.inputSequenceLabel.Name = "inputSequenceLabel";
+            this.inputSequenceLabel.Size = new System.Drawing.Size(197, 25);
+            this.inputSequenceLabel.TabIndex = 1;
+            this.inputSequenceLabel.Text = "Введите секвенцию";
+            // 
+            // resultLabel
+            // 
+            this.resultLabel.AutoSize = true;
+            this.resultLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.resultLabel.Location = new System.Drawing.Point(23, 88);
+            this.resultLabel.Name = "resultLabel";
+            this.resultLabel.Size = new System.Drawing.Size(111, 25);
+            this.resultLabel.TabIndex = 2;
+            this.resultLabel.Text = "Результат";
+            // 
+            // resultTextBox
+            // 
+            this.resultTextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.resultTextBox.Location = new System.Drawing.Point(23, 128);
+            this.resultTextBox.Multiline = true;
+            this.resultTextBox.Name = "resultTextBox";
+            this.resultTextBox.ReadOnly = true;
+            this.resultTextBox.Size = new System.Drawing.Size(401, 283);
+            this.resultTextBox.TabIndex = 3;
+            // 
+            // getResultButton
+            // 
+            this.getResultButton.Enabled = false;
+            this.getResultButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.getResultButton.Location = new System.Drawing.Point(512, 44);
+            this.getResultButton.Name = "getResultButton";
+            this.getResultButton.Size = new System.Drawing.Size(193, 64);
+            this.getResultButton.TabIndex = 4;
+            this.getResultButton.Text = "Получить";
+            this.getResultButton.UseVisualStyleBackColor = true;
+            this.getResultButton.Click += new System.EventHandler(this.getResultButton_Click);
+            // 
+            // enterU0370Button
+            // 
+            this.enterU0370Button.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.enterU0370Button.Location = new System.Drawing.Point(512, 165);
+            this.enterU0370Button.Name = "enterU0370Button";
+            this.enterU0370Button.Size = new System.Drawing.Size(70, 70);
+            this.enterU0370Button.TabIndex = 5;
+            this.enterU0370Button.Text = "Ͱ";
+            this.enterU0370Button.UseVisualStyleBackColor = true;
+            this.enterU0370Button.Click += new System.EventHandler(this.enterU0370Button_Click);
+            // 
+            // enterU02C4Button
+            // 
+            this.enterU02C4Button.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.enterU02C4Button.Location = new System.Drawing.Point(598, 165);
+            this.enterU02C4Button.Name = "enterU02C4Button";
+            this.enterU02C4Button.Size = new System.Drawing.Size(70, 70);
+            this.enterU02C4Button.TabIndex = 6;
+            this.enterU02C4Button.Text = "˄";
+            this.enterU02C4Button.UseVisualStyleBackColor = true;
+            this.enterU02C4Button.Click += new System.EventHandler(this.enterU02C4Button_Click);
+            // 
+            // enterU02C5Button
+            // 
+            this.enterU02C5Button.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.enterU02C5Button.Location = new System.Drawing.Point(512, 254);
+            this.enterU02C5Button.Name = "enterU02C5Button";
+            this.enterU02C5Button.Size = new System.Drawing.Size(70, 70);
+            this.enterU02C5Button.TabIndex = 7;
+            this.enterU02C5Button.Text = "˅";
+            this.enterU02C5Button.UseVisualStyleBackColor = true;
+            this.enterU02C5Button.Click += new System.EventHandler(this.enterU02C5Button_Click);
+            // 
+            // enterU00ACButton
+            // 
+            this.enterU00ACButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.enterU00ACButton.Location = new System.Drawing.Point(598, 254);
+            this.enterU00ACButton.Name = "enterU00ACButton";
+            this.enterU00ACButton.Size = new System.Drawing.Size(70, 70);
+            this.enterU00ACButton.TabIndex = 8;
+            this.enterU00ACButton.Text = "¬";
+            this.enterU00ACButton.UseVisualStyleBackColor = true;
+            this.enterU00ACButton.Click += new System.EventHandler(this.enterU00ACButton_Click);
+            // 
+            // enterU2192Button
+            // 
+            this.enterU2192Button.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.enterU2192Button.Location = new System.Drawing.Point(512, 341);
+            this.enterU2192Button.Name = "enterU2192Button";
+            this.enterU2192Button.Size = new System.Drawing.Size(70, 70);
+            this.enterU2192Button.TabIndex = 9;
+            this.enterU2192Button.Text = "→";
+            this.enterU2192Button.UseVisualStyleBackColor = true;
+            this.enterU2192Button.Click += new System.EventHandler(this.enterU2192Button_Click);
+            // 
+            // sequenceValidationLabel
+            // 
+            this.sequenceValidationLabel.AutoSize = true;
+            this.sequenceValidationLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.sequenceValidationLabel.Location = new System.Drawing.Point(262, 13);
+            this.sequenceValidationLabel.Name = "sequenceValidationLabel";
+            this.sequenceValidationLabel.Size = new System.Drawing.Size(232, 25);
+            this.sequenceValidationLabel.TabIndex = 10;
+            this.sequenceValidationLabel.Text = "sequenceValidationLabel";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.sequenceValidationLabel);
+            this.Controls.Add(this.enterU2192Button);
+            this.Controls.Add(this.enterU00ACButton);
+            this.Controls.Add(this.enterU02C5Button);
+            this.Controls.Add(this.enterU02C4Button);
+            this.Controls.Add(this.enterU0370Button);
+            this.Controls.Add(this.getResultButton);
+            this.Controls.Add(this.resultTextBox);
+            this.Controls.Add(this.resultLabel);
+            this.Controls.Add(this.inputSequenceLabel);
+            this.Controls.Add(this.enterSequenceTextBox);
+            this.Name = "Form1";
+            this.Text = "Sequence";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox enterSequenceTextBox;
+        private System.Windows.Forms.Label inputSequenceLabel;
+        private System.Windows.Forms.Label resultLabel;
+        private System.Windows.Forms.TextBox resultTextBox;
+        private System.Windows.Forms.Button getResultButton;
+        private System.Windows.Forms.Button enterU0370Button;
+        private System.Windows.Forms.Button enterU02C4Button;
+        private System.Windows.Forms.Button enterU02C5Button;
+        private System.Windows.Forms.Button enterU00ACButton;
+        private System.Windows.Forms.Button enterU2192Button;
+        private System.Windows.Forms.Label sequenceValidationLabel;
+    }
+}
+
---
+++ Sequence/Form1.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Sequence
+{
+    public partial class Form1 : Form
+    {
+        private int cursorPosition = 0;
+        public Form1()
+        {
+            InitializeComponent();
+            SequenceValidationLabelInit();
+            enterSequenceTextBox.Focus();
+        }
+
+        private void getResultButton_Click(object sender, EventArgs e)
+        {
+            ProofSequence();
+        }
+
+        private void ProofSequence()
+        {
+            Regex regex02 = new Regex("^(aͰa)|(bͰb)|(cͰc)|(dͰd)|(eͰe)|(fͰf)|(AͰA)|(BͰB)|(CͰC)|(DͰD)|(EͰE)|(FͰF)|(¬aͰ¬a)|(¬bͰ¬b)|(¬cͰ¬c)|(¬dͰ¬d)|(¬eͰ¬e)|(¬fͰ¬f)|(¬AͰ¬A)|(¬BͰ¬B)|(¬CͰ¬C)|(¬DͰ¬D)|(¬EͰ¬E)|(¬FͰ¬F)$");
+            resultTextBox.Text = "";
+            if (regex02.IsMatch(enterSequenceTextBox.Text))
+            {
+                resultTextBox.Text = enterSequenceTextBox.Text + Environment.NewLine + "теорема доказана" + Environment.NewLine + "так как эквивалентна" + Environment.NewLine + "ФͰФ";
+            }
+            else if (true)
+            {
+
+            }
+        }
+
+        private void enterU0370Button_Click(object sender, EventArgs e)
+        {
+            cursorPosition = enterSequenceTextBox.SelectionStart;
+            enterSequenceTextBox.Text = enterSequenceTextBox.Text.Insert(cursorPosition, "\u0370");//Ͱ
+            enterSequenceTextBox.Focus();
+            enterSequenceTextBox.Select(cursorPosition + 1, 0);
+        }
+
+        private void enterU02C4Button_Click(object sender, EventArgs e)
+        {
+            cursorPosition = enterSequenceTextBox.SelectionStart;
+            enterSequenceTextBox.Text = enterSequenceTextBox.Text.Insert(cursorPosition, "\u02C4");//˄
+            enterSequenceTextBox.Focus();
+            enterSequenceTextBox.Select(cursorPosition + 1, 0);
+        }
+
+        private void enterU02C5Button_Click(object sender, EventArgs e)
+        {
+            cursorPosition = enterSequenceTextBox.SelectionStart;
+            enterSequenceTextBox.Text = enterSequenceTextBox.Text.Insert(cursorPosition, "\u02C5");//˅
+            enterSequenceTextBox.Focus();
+            enterSequenceTextBox.Select(cursorPosition + 1, 0);
+        }
+
+        private void enterU00ACButton_Click(object sender, EventArgs e)
+        {
+            cursorPosition = enterSequenceTextBox.SelectionStart;
+            enterSequenceTextBox.Text = enterSequenceTextBox.Text.Insert(cursorPosition, "\u00AC");//¬
+            enterSequenceTextBox.Focus();
+            enterSequenceTextBox.Select(cursorPosition + 1, 0);
+        }
+
+        private void enterU2192Button_Click(object sender, EventArgs e)
+        {
+            cursorPosition = enterSequenceTextBox.SelectionStart;
+            enterSequenceTextBox.Text = enterSequenceTextBox.Text.Insert(cursorPosition, "\u2192");//→
+            enterSequenceTextBox.Focus();
+            enterSequenceTextBox.Select(cursorPosition + 1, 0);
+        }
+
+        private void SequenceValidationLabelInit()
+        {
+            sequenceValidationLabel.Text = "Введите секвецию";
+            sequenceValidationLabel.ForeColor = Color.Green;
+            getResultButton.Enabled = false;
+        }
+
+        private void enterSequenceTextBox_TextChanged(object sender, EventArgs e)
+        {
+            string sequencePattern = @"^((\(*(¬)?\(*[abcdefABCDEF]\)*(,\(*(¬)?\(*[abcdefABCDEF]\)*)*([˄˅→]\(*(¬)?\(*[abcdefABCDEF]\)*(,\(*(¬)?\(*[abcdefABCDEF]\)*)*)*)?Ͱ(\(*(¬)?\(*[abcdefABCDEF]\)*(,\(*(¬)?\(*[abcdefABCDEF]\)*)*([˄˅→]\(*(¬)?\(*[abcdefABCDEF]\)*(,\(*(¬)?\(*[abcdefABCDEF]\)*)*)*)?(;\s*)?)*$";
+            string multiEqualsPattern = @"Ͱ{2,}";
+            string emptyEqualsPattern = @"^Ͱ$";
+            Regex regexSequencePattern = new Regex(sequencePattern);
+            Regex regexMultiEqualsPattern = new Regex(multiEqualsPattern);
+            Regex regexEmptyEqualsPattern = new Regex(emptyEqualsPattern);
+            if (!regexEmptyEqualsPattern.IsMatch(enterSequenceTextBox.Text) &&
+                (enterSequenceTextBox.Text.ToCharArray().Where(ch => ch == 'Ͱ').Count() - enterSequenceTextBox.Text.ToCharArray().Where(ch => ch == ';').Count() <= 1) &&
+                IsWriteBrackets(enterSequenceTextBox.Text) && //(enterSequenceTextBox.Text.ToCharArray().Where(ch => ch == '(').Count() - enterSequenceTextBox.Text.ToCharArray().Where(ch => ch == ')').Count() == 0) &&
+                !regexMultiEqualsPattern.IsMatch(enterSequenceTextBox.Text) &&
+                regexSequencePattern.IsMatch(enterSequenceTextBox.Text))
+            {
+                sequenceValidationLabel.ForeColor = Color.Green;
+                sequenceValidationLabel.Text = "Секвенция принимается";
+                getResultButton.Enabled = true;
+            }
+            else
+            {
+                sequenceValidationLabel.ForeColor = Color.Red;
+                sequenceValidationLabel.Text = "Секвенция не принимается";
+                getResultButton.Enabled = false;
+            }
+            if (enterSequenceTextBox.Text == "")
+            {
+                SequenceValidationLabelInit();
+            }
+        }
+
+        private bool IsWriteBrackets(string str)
+        {
+            if (!(enterSequenceTextBox.Text.ToCharArray().Where(ch => ch == '(').Count() - enterSequenceTextBox.Text.ToCharArray().Where(ch => ch == ')').Count() == 0))
+            {
+                return false;
+            }
+            string[] subStrs = str.Split('Ͱ', ';', ' ');
+            Regex regex01 = new Regex(@"\(*[abcdefABCDEF]\)*([˄˅→]\(*[abcdefABCDEF])?\)*");
+            foreach (string subStr in subStrs.Where(s => s.Length != 0))
+            {
+                if (!(subStr.ToCharArray().Where(ch => ch == '(').Count() - subStr.ToCharArray().Where(ch => ch == ')').Count() == 0) || !regex01.IsMatch(subStr))
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        private bool IsLeftSideEqualsRightSide(string str)
+        {
+            string[] subStrs = str.Split('Ͱ');
+            if (subStrs.Length != 2)
+            {
+                return false;
+            }
+            if (subStrs[0] == subStrs[1])
+            {
+                return true;
+            }
+            return false;
+        }
+    }
+}
+// TODO: скобки в разных местах валидация!!!
